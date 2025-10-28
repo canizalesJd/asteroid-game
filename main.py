202033757f2e105618a6c1ca3670f7c3c2f71c80
canizalesJd <canizalesjd@gmail.com>
2025-10-27 23:33:16 -0600

    Adding player class

--- main.py
+++ main.py
@@ -1,22 +1,24 @@
 import pygame
 from constants import *
 
-def main():
-    # Initialize dependency
-    pygame.init()
-    # Set up GUI window size
-    screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 
-    # Game inifinite loop
+def main():
+    pygame.init()
+    screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    clock = pygame.time.Clock()
+    dt = 0
+
     while True:
-        # Turn the screen black
-        screen.fill("black")
-        # Refresh the screen
-        pygame.display.flip()
-        # Handle events
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
+
+        screen.fill("black")
+        pygame.display.flip()
+
+        # limit the framerate to 60 FPS
+        dt = clock.tick(60) / 1000
+
 
 if __name__ == "__main__":
     main()
